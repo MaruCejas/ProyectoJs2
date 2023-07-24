fa813a1a2aca25850d67ae9a0275b2507c9a9e80
Mariela Cejas <marualecejas@hotmail.com>
2023-07-24 19:11:59 -0300

    index.html

---
+++ codigo.js
@@ -0,0 +1,15 @@
+const btn = document.createElement( 'button');
+btn.textContent = 'Agregar';
+document.body.append(btn) ;
+
+btn.addEventListener('click', () => {
+    console.log('click');
+    const btn = document.createElement('button');
+    btn.textContent = 'Agregar';
+    document.body.append (btn);
+    const p = document.createElement ('p');
+    
+
+
+
+})
