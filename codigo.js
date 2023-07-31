58dc132efb9d2c251a0a88e4174bb2a11f6f0c6b
MaruCejas <133709427+MaruCejas@users.noreply.github.com>
2023-07-31 09:54:14 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,15 +1,35 @@
-const btn = document.createElement( 'button');
-btn.textContent = 'Agregar';
-document.body.append(btn) ;
+var  formulario = document.getElementsByName('fomulario') [0]
+     elementos = formulario.elements ,
+     boton = document.getElementById('btn');
 
-btn.addEventListener('click', () => {
-    console.log('click');
-    const btn = document.createElement('button');
-    btn.textContent = 'Agregar';
-    document.body.append (btn);
-    const p = document.createElement ('p');
-    
+var  validarNombre = function(){
+     if (formulario.nombre.value == 0){
+     alert( "Completa el campo nombre");
+     e.preventDefault();
+    }
+};
+
+var validarRadio = function(e){
+    if (formulario.sexo[0].checked == true || formulario.sexo[1].checked == true){
+    } else {
+        alert("Completa el campo sexo");
+        e.preventDefault();
+    }
+};
+
+var validarCheckbox = function(e){
+    if (formulario.terminos.checked == false) {
+        alert("Acepta los terminos");
+        e.preventDefault();
+    }
+};
+
+var  validar = function(e){
+    validarNombre(e);
+    validarRadio(e);
+    validarCheckbox(e);
+}
 
 
+formulario.addEvenlistener("submit" , validar);
 
-})
